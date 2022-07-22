948923cd47e66a68dfcdc6a71e3c4ac54a31e0af
Tomomi-K1 <tomomi.music@gmail.com>
2022-07-21 19:24:52 -0700

    added more functions to make code readable

--- app.js
+++ app.js
@@ -5,34 +5,40 @@ console.log("Let's get this party started!");
 // Allow the user to search for as many GIFs as they would like and keep appending them to the page
 // Allow the user to remove all of the GIFs by clicking a button
 
-// data.bitly_gif_url
-
-// API Key// 1dPiG58K2PcUMJDxOSdVtquEGRwRLDrI
 
 const searchBtn = document.querySelector('#searchBtn');
 const input = document.querySelector('#searchTerm');
 const imageArea = document.querySelector('#gif');
 const removeBtn = document.querySelector('#remove');
+const apiKey = '1dPiG58K2PcUMJDxOSdVtquEGRwRLDrI'; //it's better to save api keys in variable so url in get request is more understandable
+let NumOfGifJsonData = 30; // it's better to put random number in variable so that you know what that number does.
 
+async function getGifUrl(searchTerm){
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=${NumOfGifJsonData}&offset=0&rating=g&lang=en`);
+    console.log("res",res);
+    const randomNum = Math.floor(Math.random()*NumOfGifJsonData);
+    let gifUrl=res.data.data[randomNum].images.original.webp;
+    
+    createImgElemAndAppendGifToImageArea(gifUrl);
+}
+
+//this function will accept url from getGifUrl function
+function createImgElemAndAppendGifToImageArea(gifUrl){
+    const newElem = document.createElement('img');
+    newElem.src = gifUrl;
+    imageArea.append(newElem);
+}
+
+//click search button and gif will appear.
 searchBtn.addEventListener('click', function(e){
     e.preventDefault();
-    getGif(input.value);
+    getGifUrl(input.value);
     input.value ='';
     // console.log(input.value)
     
 })
 
-async function getGif(searchTerm){
-    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=1dPiG58K2PcUMJDxOSdVtquEGRwRLDrI&q=${searchTerm}&limit=30&offset=0&rating=g&lang=en`);
-    console.log("res",res);
-    const randomNum = Math.floor(Math.random()*30)
-    console.log("randomNum", randomNum);
-    const newImg = document.createElement('img');
-    newImg.src = res.data.data[randomNum].images.original.webp;
-    imageArea.append(newImg);
-    // console.log(res.data.data[0].url)
-}
-
+//add eventlistner to remove all button to clear all images
 removeBtn.addEventListener('click', function(e){
     e.preventDefault();
     const allImgs = document.querySelectorAll('img');
@@ -40,9 +46,5 @@ removeBtn.addEventListener('click', function(e){
         img.remove();
     }
 })
-
-
-
-
 
 
